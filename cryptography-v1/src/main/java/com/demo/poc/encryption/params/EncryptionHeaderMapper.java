8def1c4035d1fae299611edb82312563b5cc2ff5
Miguel Armas <marmasa@proveedor.intercorp.com.pe>
2025-04-25 23:01:47 -0500

    Update Tracing

--- cryptography-v1/src/main/java/com/demo/poc/encryption/params/EncryptionHeaderMapper.java
+++ cryptography-v1/src/main/java/com/demo/poc/encryption/params/EncryptionHeaderMapper.java
@@ -1,5 +1,7 @@
 package com.demo.poc.encryption.params;
 
+import com.demo.poc.commons.core.tracing.enums.ForwardedParam;
+import com.demo.poc.commons.core.tracing.enums.TraceParam;
 import com.demo.poc.commons.core.validations.ParamMapper;
 import org.springframework.stereotype.Component;
 
@@ -11,8 +13,8 @@ public class EncryptionHeaderMapper implements ParamMapper {
     @Override
     public Object map(Map<String, String> params) {
         EncryptionHeader headers = new EncryptionHeader();
-        headers.setTraceId(params.get("trace-id"));
-        headers.setChannelId(params.get("channel-id"));
+        headers.setTraceParent(params.get(TraceParam.TRACE_PARENT.getKey()));
+        headers.setChannelId(params.get(ForwardedParam.CHANNEL_ID.getKey()));
         headers.setFeature(params.get("feature"));
         headers.setEncryptionMethod(params.get("encryption-method"));
 
