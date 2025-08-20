3feebaee27b1a6197f78672f6b6c9dec3d85afb2
Miguel <miguel.armas.abt@gmail.com>
2025-08-20 14:58:02 -0500

    Update RestClientTemplate

--- customer-v1/src/main/java/com/demo/poc/customer/repository/cryptography/CryptographyRepository.java
+++ customer-v1/src/main/java/com/demo/poc/customer/repository/cryptography/CryptographyRepository.java
@@ -1,18 +1,15 @@
 package com.demo.poc.customer.repository.cryptography;
 
+import com.demo.poc.commons.core.errors.dto.ErrorDto;
 import com.demo.poc.commons.core.properties.restclient.RestClient;
 import com.demo.poc.commons.core.restclient.RestClientTemplate;
-import com.demo.poc.commons.core.restclient.dto.ExchangeRequest;
-import com.demo.poc.commons.core.restclient.utils.HeadersFiller;
 import com.demo.poc.commons.custom.properties.ApplicationProperties;
 import com.demo.poc.customer.repository.cryptography.wrapper.request.DecryptionRequestWrapper;
 import com.demo.poc.customer.repository.cryptography.wrapper.request.EncryptionRequestWrapper;
 import com.demo.poc.customer.repository.cryptography.wrapper.response.DecryptionResponseWrapper;
 import com.demo.poc.customer.repository.cryptography.wrapper.response.EncryptionResponseWrapper;
 import lombok.RequiredArgsConstructor;
-import org.springframework.http.HttpMethod;
 import org.springframework.stereotype.Repository;
-
 import java.util.Map;
 
 @Repository
@@ -25,28 +22,28 @@ public class CryptographyRepository {
     private final ApplicationProperties properties;
 
     public String encrypt(Map<String, String> headers, String value) {
-        RestClient restClient = properties.getRestClients().get(SERVICE_NAME);
+        RestClient restClient = properties.searchRestClient(SERVICE_NAME);
 
-        return restTemplate.exchange(ExchangeRequest.<EncryptionRequestWrapper,EncryptionResponseWrapper>builder()
-                .url(restClient.getRequest().getEndpoint() + "/encrypt")
-                .httpMethod(HttpMethod.POST)
-                .requestBody(EncryptionRequestWrapper.builder().value(value).build())
-                .responseClass(EncryptionResponseWrapper.class)
-                .headers(HeadersFiller.fillHeaders(restClient.getRequest().getHeaders(), headers))
-                .build(), SERVICE_NAME)
-                .getCipherMessage();
+        return restTemplate
+            .service(SERVICE_NAME)
+            .post()
+            .uri(restClient.getRequest().getEndpoint() + "/encrypt")
+            .headers(headers)
+            .body(EncryptionRequestWrapper.builder().value(value).build())
+            .retrieve(EncryptionResponseWrapper.class, ErrorDto.class)
+            .getCipherMessage();
     }
 
     public String decrypt(Map<String, String> headers, String cipherMessage) {
-        RestClient restClient = properties.getRestClients().get(SERVICE_NAME);
+        RestClient restClient = properties.searchRestClient(SERVICE_NAME);
 
-        return restTemplate.exchange(ExchangeRequest.<DecryptionRequestWrapper,DecryptionResponseWrapper>builder()
-                .url(restClient.getRequest().getEndpoint() + "/decrypt")
-                .httpMethod(HttpMethod.POST)
-                .requestBody(DecryptionRequestWrapper.builder().cipherMessage(cipherMessage).build())
-                .responseClass(DecryptionResponseWrapper.class)
-                .headers(HeadersFiller.fillHeaders(restClient.getRequest().getHeaders(), headers))
-                .build(), SERVICE_NAME)
-                .getValue();
+        return restTemplate
+            .service(SERVICE_NAME)
+            .post()
+            .uri(restClient.getRequest().getEndpoint() + "/decrypt")
+            .headers(headers)
+            .body(DecryptionRequestWrapper.builder().cipherMessage(cipherMessage).build())
+            .retrieve(DecryptionResponseWrapper.class, ErrorDto.class)
+            .getValue();
     }
 }
